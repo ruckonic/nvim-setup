1245e2498d3c3ab779006d4b752a6f977c9ade55
Rene Corrales <ruckonic@icloud.com>
2024-05-10 10:07:27 -0600

    telescope mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,9 +3,18 @@ local telescope = require('telescope')
 local telscope_actions = require('telescope.actions')
 local trouble = require('trouble.providers.telescope')
 
-telescope.setup();
-vim.keymap.set('i', '<c-t>', trouble.open_with_trouble, {})
-vim.keymap.set('n', '<c-t>', trouble.open_with_trouble, {})
+telescope.setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-t>"] = trouble.open_with_trouble,
+            },
+            n = {
+                ["<C-t>"] = trouble.open_with_trouble,
+            }
+        },
+    },
+});
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
